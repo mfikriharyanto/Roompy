643482595bb91209f7fbc50bf9f9126896aee875
Muhammad Fikri Haryanto <muhammad.fikri06@ui.ac.id>
2022-12-02 01:26:03 +0700

    Update related_name user models

--- user/models.py
+++ user/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 	email = models.EmailField(unique=True)
-	follower_user = models.ManyToManyField('self', blank=True, related_name='follower', symmetrical=False)
-	following_user = models.ManyToManyField('self', blank=True, related_name='following', symmetrical=False)
+	follower_user = models.ManyToManyField('self', blank=True, related_name='follower_users', symmetrical=False)
+	following_user = models.ManyToManyField('self', blank=True, related_name='following_users', symmetrical=False)
 	following_room = models.ManyToManyField(to='room.Room', blank=True)
 
 	def add_following(self, user):
