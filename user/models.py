e0c9de4ddc81e152a829b010303377013b40e7a1
Muhammad Fikri Haryanto <muhammad.fikri06@ui.ac.id>
2022-12-01 23:34:55 +0700

    Update user models

--- user/models.py
+++ user/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 	email = models.EmailField(unique=True)
-	following_room = models.ManyToManyField(to='room.Room')
+	follower_user = models.ManyToManyField('self', blank=True, related_name='follower', symmetrical=False)
+	following_user = models.ManyToManyField('self', blank=True, related_name='following', symmetrical=False)
+	following_room = models.ManyToManyField(to='room.Room', blank=True)
