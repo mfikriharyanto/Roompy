dfd8763f9298705cc7a15bb9190b3bcd863ec5f3
Muhammad Fikri Haryanto <muhammad.fikri06@ui.ac.id>
2022-12-01 23:37:31 +0700

    Add api follow user

--- user/api/urls.py
+++ user/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.getUsers),
+  path('<str:pk>/follow', views.postFollowUser),
 ]
--- user/api/views.py
+++ user/api/views.py
@@ -8,3 +8,23 @@ def getUsers(request):
   users = User.objects.all()
   serializer = UserSerializer(users, many=True)
   return Response(serializer.data)
+
+@api_view(['POST'])
+def postFollowUser(request, pk):
+  try:
+    followerUser = User.objects.get(id = request.user.id)
+    followingUser = User.objects.get(id = pk)
+
+    if followerUser == followingUser:
+      return Response({'message': 'You can\'t follow your own account'}, status=400)
+
+    if followerUser.following_user.filter(id = pk).exists():
+      return Response({'message': f'You\'ve followed {followingUser}'}, status=400)
+
+    followerUser.following_user.add(followingUser)
+    followingUser.follower_user.add(followerUser)
+      
+  except User.DoesNotExist:
+    return Response({'message': f'User with id {pk} does not exist'}, status=404)
+
+  return Response({'message': f'Success follow {followingUser}'}, status=200)
