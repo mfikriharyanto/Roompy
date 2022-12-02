8d0f4bb302cef7333b1ff439da24a8fdca5eb0dc
Muhammad Fikri Haryanto <muhammad.fikri06@ui.ac.id>
2022-12-02 09:48:15 +0700

    Add get top user path

--- user/api/urls.py
+++ user/api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.get_all_users),
+  path('top-users', views.get_top_users),
   path('<str:pk>/follow', views.post_follow_user),
   path('<str:pk>/unfollow', views.post_unfollow_user),
 ]
