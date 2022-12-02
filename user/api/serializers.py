b95f0f0c826f36d755a0ed7005e2080d98578aac
Muhammad Fikri Haryanto <muhammad.fikri06@ui.ac.id>
2022-12-02 09:49:53 +0700

    Update user serializers

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -4,4 +4,4 @@ from user.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'total_follower_user', 'total_following_user']
