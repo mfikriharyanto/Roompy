25778ef5f5d897fe799812d5503816065baf3cda
haikal.rahman <haikal.rahman@ui.ac.id>
2022-12-10 20:35:34 +0700

    Update edit profile

--- user/views.py
+++ user/views.py
@@ -20,17 +20,23 @@ def get_user(request, pk):
 
 def edit_user(request, pk):
     if request.method == 'POST':
-        form = ProfileForm(request.POST)
+        form = ProfileForm(request.POST, instance=request.user)
         if form.is_valid():
             try:
                 form.save()
             except:
                 return show_failure_notif("Gagal menyimpan perubahan. Mohon dicoba kembali beberapa saat lagi.")
             return HttpResponseRedirect(request.path_info)
-        return
     else:
         form = ProfileForm()
-    return render(request, 'edit_profile.html', {'form':form})
+        user = UserManager.get_user(pk)
+        if user == None:
+            return HttpResponseRedirect(request.path_info)
+        context = {
+            'user': user,
+            'form':form
+        }
+    return render(request, 'profile_form.html', context)
 
 def show_failure_notif(args):
     return JsonResponse({'status':'false', 'message':args}, status=400)
