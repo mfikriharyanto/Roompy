b6a6fe9fe6ddd821d10fbd179533075609f3b57e
yuniaharmulyati <yunia.harmulyati11@ui.ac.id>
2022-12-10 16:38:39 +0700

    yunia/topics - add detail topics and reformat html

--- topics/urls.py
+++ topics/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create/',views.create_topic, name='create-topic'),
     path('all-topic/',views.all_topic, name='all-topic'),
     path('trending-topic/',views.trending_topic, name='trending-topic'),
+    path('<str:pk>/', views.detail_topic, name='detail-topic'),
 ]
